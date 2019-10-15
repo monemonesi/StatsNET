20646655f44a66f73148c12ed6727f8423059eeb
Roberto Monesi <roberto.monesi@outlook.com>
2019-10-15 08:42:26 +0100

    Writing failing test for GetAbsoluteFrequencies

--- StatsNET/NUnitTestProject1/StatisticHelperTests.cs
+++ StatsNET/NUnitTestProject1/StatisticHelperTests.cs
@@ -10,9 +10,10 @@ namespace Statistic.Basic.UnitTests
     [TestFixture]
     public class StatisticHelperTests
     {
-        IList<double> _dataSet;
+        IList<double> _dataSet, _intervals;
         double _startInterval, _endInterval;
-        int _result;
+        int _absoluteFrequency;
+        List<int> _absoluteFrequencies;
 
         //28, 35, 42, 90, 70, 56, 75, 66, 30, 89, 75, 64, 81, 69, 55, 83, 72, 68, 73, 16
         [TestCase("1,3,5", 0.0, 3.0, 1)]
@@ -20,9 +21,7 @@ namespace Statistic.Basic.UnitTests
         [TestCase("1,3,5", 0.0, 5.5, 3)]
         [TestCase("-1,0,1,3,5", 0.0, 5.5, 4)]
         [TestCase("-1,0,1,3,5", -1.0, 3, 3)]
-
-
-        public void GetAbsoluteFrequencyShouldReturnTheCorrectFrequencyForASpecificInterval(string data, double startInterval, double endInterval, int expected)
+        public void GetAbsoluteFrequency(string data, double startInterval, double endInterval, int expected)
         {
             GivenASetOfData(data);
             GivenASpecificRange(startInterval, endInterval);
@@ -30,14 +29,41 @@ namespace Statistic.Basic.UnitTests
             ThenItShouldReturnTheCorrectAbsoluteFrequency(expected);
         }
 
+        [TestCase("1,3,5", "0.0,4.0,10.0", "2,1")]
+        public void GetAbsoluteFrequencies(string data, string intervals, string expected)
+        {
+            GivenASetOfData(data);
+            GivenASeriesOfValuesAsIntervals(intervals);
+            WhenGetAbsoluteFrequenciesIsCalled();
+            ThenItShouldReturnTheCorrectFrequencyForEachInterval(expected);
+        }
+
+        private void WhenGetAbsoluteFrequenciesIsCalled()
+        {
+            _absoluteFrequencies = _dataSet.GetAbsoluteFrequenciesForIntervals(_intervals);
+        }
+
+        private void ThenItShouldReturnTheCorrectFrequencyForEachInterval(string expectedAsString)
+        {
+            List<int> expected = ParseStringToListOfInt(expectedAsString);
+            Assert.AreEqual(expected, _absoluteFrequencies);
+        }
+
+  
+
+        private void GivenASeriesOfValuesAsIntervals(string intervals)
+        {
+            _intervals = ParseStringToListOfDouble(intervals);
+        }
+
         private void ThenItShouldReturnTheCorrectAbsoluteFrequency(int expected)
         {
-            Assert.AreEqual(expected, _result);
+            Assert.AreEqual(expected, _absoluteFrequency);
         }
 
         private void WhenGetAbsoluteFrequencyIsCalled()
         {
-            _result = _dataSet.GetAbsoluteFrequencyInRange(_startInterval, _endInterval);
+            _absoluteFrequency = _dataSet.GetAbsoluteFrequencyForInterval(_startInterval, _endInterval);
         }
 
         private void GivenASpecificRange(double startInterval, double endInterval)
@@ -48,9 +74,17 @@ namespace Statistic.Basic.UnitTests
 
         private void GivenASetOfData(string data)
         {
-            _dataSet = data.Split(',').Select(double.Parse).ToList();
+            _dataSet = ParseStringToListOfDouble( data);
         }
 
+        private List<double> ParseStringToListOfDouble(string dataAsString)
+        {
+            return dataAsString.Split(',').Select(double.Parse).ToList();
+        }
 
+        private List<int> ParseStringToListOfInt(string dataAsString)
+        {
+            return dataAsString.Split(',').Select(int.Parse).ToList();
+        }
     }
 }
