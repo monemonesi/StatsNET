b66d5aa6674bb36f2f49c953e040816167425ae9
Roberto Monesi <roberto.monesi@outlook.com>
2019-10-15 08:18:10 +0100

    Adding more tests

--- StatsNET/NUnitTestProject1/StatisticHelperTests.cs
+++ StatsNET/NUnitTestProject1/StatisticHelperTests.cs
@@ -16,6 +16,12 @@ namespace Statistic.Basic.UnitTests
 
         //28, 35, 42, 90, 70, 56, 75, 66, 30, 89, 75, 64, 81, 69, 55, 83, 72, 68, 73, 16
         [TestCase("1,3,5", 0.0, 3.0, 1)]
+        [TestCase("1,3,5", 0.0, 0.5, 0)]
+        [TestCase("1,3,5", 0.0, 5.5, 3)]
+        [TestCase("-1,0,1,3,5", 0.0, 5.5, 4)]
+        [TestCase("-1,0,1,3,5", -1.0, 3, 3)]
+
+
         public void GetAbsoluteFrequencyShouldReturnTheCorrectFrequencyForASpecificInterval(string data, double startInterval, double endInterval, int expected)
         {
             GivenASetOfData(data);
