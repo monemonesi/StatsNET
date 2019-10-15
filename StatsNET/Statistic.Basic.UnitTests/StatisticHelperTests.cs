d81574a4cddf030a3fd09b52a7e9733d065bce64
Roberto Monesi <roberto.monesi@outlook.com>
2019-10-15 07:54:25 +0100

    Implementing basic version ECDF

--- StatsNET/Statistic.Basic.UnitTests/StatisticHelperTests.cs
+++ StatsNET/Statistic.Basic.UnitTests/StatisticHelperTests.cs
@@ -17,7 +17,7 @@ namespace Statistic.Basic.UnitTests
 
         //28, 35, 42, 90, 70, 56, 75, 66, 30, 89, 75, 64, 81, 69, 55, 83, 72, 68, 73, 16
         [TestCase("1,3,5",0,3,2)]
-        public void GetAbsoluteFrequencyShouldReturnTheCorrectFrequencyForASpecificInterval(string data, double startInterval, double endInterval, double expected)
+        public void GetAbsoluteFrequencyShouldReturnTheCorrectFrequencyForASpecificInterval(string data, double startInterval, double endInterval, int expected)
         {
             GivenASetOfData(data);
             GivenASpecificRange(startInterval, endInterval);
--- StatsNET/Statistic.Basic/StatisticHelper.cs
+++ StatsNET/Statistic.Basic/StatisticHelper.cs
@@ -1,8 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Statistic.Basic
 {
-    public class StatisticHelper
+    public static class StatisticHelper
     {
+        public static int GetAbsoluteFrequencyInRange(this IList<double> dataset, double start, double end)
+        {
+            int result = 0;
+
+            foreach(var data in dataset)
+            {
+                if (DataIsInRange(data, start, end))
+                    result++;
+            }
+
+            return result;
+        }
+
+        private static bool DataIsInRange(double data, double start, double end)
+        {
+            return data >= start && data < end;
+        }
     }
 }
