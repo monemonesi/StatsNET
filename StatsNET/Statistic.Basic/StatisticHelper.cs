230eed8df582b9bbbb2eeb977e0c89076ee68dad
Roberto Monesi <roberto.monesi@outlook.com>
2019-10-15 08:15:32 +0100

    Add doc

--- StatsNET/Statistic.Basic/StatisticHelper.cs
+++ StatsNET/Statistic.Basic/StatisticHelper.cs
@@ -5,6 +5,13 @@ namespace Statistic.Basic
 {
     public static class StatisticHelper
     {
+        /// <summary>
+        /// Given a dataset finds the absolute frequency for a specified range. The upper bound is exclusive
+        /// </summary>
+        /// <param name="dataset"></param>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <returns></returns>
         public static int GetAbsoluteFrequencyInRange(this IList<double> dataset, double start, double end)
         {
             int result = 0;
