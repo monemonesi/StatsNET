3ae9943cce821b2c754f15150ab3c62a5355cdca
Roberto Monesi <roberto.monesi@outlook.com>
2019-10-21 15:37:06 +0100

    implementic basic version of arithmetica mean

---
+++ StatsNET/Statistic.Basic.Tests/MeasureOfCentralTendecyCalculatorTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Statistic.Basic.Tests
+{
+    [TestFixture]
+    class MeasureOfCentralTendecyCalculatorTests : BaseTestClass
+    {
+        private double _mean;
+
+        [TestCase("22,24,21,30,28,29", 25.66667)]
+        public void ArithmeticMeanShouldReturnTheCorrectValue(string data, double expected)
+        {
+            GivenASetOfData(data);
+            WhenMeanIsCalled();
+            ThenItShouldReturnTheCorrectArithmeticMean(expected);
+        }
+
+        private void ThenItShouldReturnTheCorrectArithmeticMean(double expected)
+        {
+            Assert.AreEqual(expected, _mean);
+        }
+
+        private void WhenMeanIsCalled()
+        {
+            _mean = Math.Round(_dataSet.Mean(),5);
+        }
+
+
+    }
+}
---
+++ StatsNET/Statistic.Basic/MeasureOfCentralTendecyCalculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Statistic.Basic
+{
+    public static class MeasureOfCentralTendecyCalculator
+    {
+        public static double Mean(this IList<double> dataset)
+        {
+            double mean = dataset.Sum() / dataset.Count; 
+
+            return mean;
+        }
+    }
+}
