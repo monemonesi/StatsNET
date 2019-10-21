da0336457ab5ff9f9914ebed397c8a6bd384c49a
Roberto Monesi <roberto.monesi@outlook.com>
2019-10-21 15:48:58 +0100

    adding more tests to arithmetic mean

--- StatsNET/Statistic.Basic.Tests/MeasureOfCentralTendecyCalculatorTests.cs
+++ StatsNET/Statistic.Basic.Tests/MeasureOfCentralTendecyCalculatorTests.cs
@@ -11,6 +11,8 @@ namespace Statistic.Basic.Tests
         private double _mean;
 
         [TestCase("22,24,21,30,28,29", 25.66667)]
+        [TestCase("1,1,1,1",1)]
+        [TestCase("22,24,21,22,25,26,25,24,23,25,25,26,27,25,26",24.4)]
         public void ArithmeticMeanShouldReturnTheCorrectValue(string data, double expected)
         {
             GivenASetOfData(data);
