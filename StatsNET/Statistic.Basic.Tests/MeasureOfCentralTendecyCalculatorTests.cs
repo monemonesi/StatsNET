947a127002b6b16e8a1131e59513c9cf2f4383c5
Roberto Monesi <roberto.monesi@outlook.com>
2019-10-21 16:51:53 +0100

    added arithmeticMeanProperty tests

--- StatsNET/Statistic.Basic.Tests/MeasureOfCentralTendecyCalculatorTests.cs
+++ StatsNET/Statistic.Basic.Tests/MeasureOfCentralTendecyCalculatorTests.cs
@@ -21,6 +21,16 @@ namespace Statistic.Basic.Tests
             ThenItShouldReturnTheExpectedValue(_mean,expected);
         }
 
+        [TestCase("22,24,21,30,28,29")]
+        [TestCase("1,1,1,1")]
+        [TestCase("22,24,21,22,25,26,25,24,23,25,25,26,27,25,26")]
+        public void ArithmeticMeanProperties(string data)
+        {
+            GivenASetOfData(data);
+            WhenMeanIsCalled();
+            ThenTheSumOfTheDeviationOfEachVariableShoulbBeZero();
+        }
+
         [TestCase("22.5,27.5,32.5","0.37, 0.58, 0.05",25.9)]
         public void WeightedMeanShouldReturnTheCorrectValue(string data, string relativeFrequencies, double expected)
         {
@@ -36,8 +46,18 @@ namespace Statistic.Basic.Tests
         {
             GivenASetOfData(data);
             GivenTheRespectiveRelativeFrequecies(relativeFrequencies);
-            //WhenWeightedMeanIsCalled();
             ThenAnExceptionShouldBeThrown();
+        }
+
+        private void ThenTheSumOfTheDeviationOfEachVariableShoulbBeZero()
+        {
+            double deviationsSum = 0;
+            foreach (var data in _dataSet)
+            {
+                deviationsSum += (data - _mean);
+            }
+
+            Assert.AreEqual(0, Math.Round(deviationsSum, 4));
         }
 
         private void ThenAnExceptionShouldBeThrown()
