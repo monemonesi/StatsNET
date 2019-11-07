035a346365a227cba2b77e1b869f9a6da5f35c4f
Roberto Monesi <roberto.monesi@outlook.com>
2019-11-07 06:09:05 +0000

    Added more test to edge cases

--- StatsNET/Statistic.Basic.Tests/MeasureOfCentralTendecyCalculatorTests.cs
+++ StatsNET/Statistic.Basic.Tests/MeasureOfCentralTendecyCalculatorTests.cs
@@ -97,7 +97,27 @@ namespace Statistic.Basic.Tests
         }
 
         [TestCase("22,24,21,30,35", "0.25,0.75" ,"22,30")]
+        [TestCase("22.5,24.3,21.6,30.6,35.7", "0", "21.6")]
+        [TestCase("22.5,24.3,21.6,30.6,35.7", "1", "35.7")]
+        [TestCase("22,22,22,22", "0", "22")]
+        [TestCase("22", "0", "22")]
+        [TestCase("22", "1", "22")]
         public void QuantileShouldAllowCustomOption(string data, string percentile, string expected)
+        {
+            _dataSet = GivenASetOfData(data);
+            _percentages = GivenASetOfData(percentile);
+            _expectedValues = GivenASetOfData(expected);
+
+            _quantiles = WhenQuantileAreCalculatedWithSpecificPercentages();
+
+            _quantiles.Should().Equal(_expectedValues);
+        }
+
+        [TestCase("22,-24,-21,30,35", "0.25,0.75", "-21,30")]
+        [TestCase("22.5,24.3,21.6,30.6,-35.7", "0", "-35.7")]
+        [TestCase("22.5,-24.3,-21.6,30.6,35.7", "1", "35.7")]
+        [TestCase("-22,-22,-22,-22", "0", "-22")]
+        public void QuatileShouldReturnTheCorrectValueWhenNegativeValueInDataset(string data, string percentile, string expected)
         {
             _dataSet = GivenASetOfData(data);
             _percentages = GivenASetOfData(percentile);
