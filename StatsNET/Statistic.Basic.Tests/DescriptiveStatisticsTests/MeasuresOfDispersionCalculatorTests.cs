ee0736d2de052654e5958c0a273ed8fc97e9f995
Roberto Monesi <roberto.monesi@outlook.com>
2019-11-10 12:01:21 +0000

    added initial boiler platte for MeasuresOfDispersion

---
+++ StatsNET/Statistic.Basic.Tests/DescriptiveStatisticsTests/MeasuresOfDispersionCalculatorTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Statistic.Basic.DescriptiveStatistics;
+
+namespace Statistic.Basic.Tests.DescriptiveStatisticsTests
+{
+    [TestFixture]
+    class MeasuresOfDispersionCalculatorTests
+    {
+    }
+}
---
+++ StatsNET/Statistic.Basic/DescriptiveStatistics/MeasuresOfDispersionCalculator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Statistic.Basic.DescriptiveStatistics
+{
+    public static class MeasuresOfDispersionCalculator
+    {
+    }
+}
