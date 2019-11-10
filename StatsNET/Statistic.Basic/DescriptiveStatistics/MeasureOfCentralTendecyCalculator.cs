1ee6c5d6eeb4a13506acc8f25cd19848b3ad8efa
Roberto Monesi <roberto.monesi@outlook.com>
2019-11-10 11:55:15 +0000

    refactored namespaces

--- StatsNET/Statistic.Basic/DescriptiveStatistics/MeasureOfCentralTendecyCalculator.cs
+++ StatsNET/Statistic.Basic/DescriptiveStatistics/MeasureOfCentralTendecyCalculator.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Statistic.Basic
+namespace Statistic.Basic.DescriptiveStatistics
 {
     public static class MeasureOfCentralTendecyCalculator
     {
