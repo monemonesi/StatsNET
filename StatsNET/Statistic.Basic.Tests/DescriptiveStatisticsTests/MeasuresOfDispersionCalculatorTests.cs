212689bb01fd71f1acdcf2af533638699487be28
Roberto Monesi <roberto.monesi@outlook.com>
2019-11-12 05:49:36 +0000

    implemented range function

--- StatsNET/Statistic.Basic.Tests/DescriptiveStatisticsTests/MeasuresOfDispersionCalculatorTests.cs
+++ StatsNET/Statistic.Basic.Tests/DescriptiveStatisticsTests/MeasuresOfDispersionCalculatorTests.cs
@@ -7,7 +7,25 @@ using Statistic.Basic.DescriptiveStatistics;
 namespace Statistic.Basic.Tests.DescriptiveStatisticsTests
 {
     [TestFixture]
-    class MeasuresOfDispersionCalculatorTests
+    class MeasuresOfDispersionCalculatorTests : BaseTestClassHelper
     {
+        private double _range;
+
+        [TestCase("4,-2,5,0", 7)]
+        [TestCase("6,6,6,6",0)]
+        [TestCase("0,1.5,-0.5,10.98",11.48)]
+        public void RangeShouldReturnTheCorrectValue(string data, double expected)
+        {
+            _dataSet = GivenASetOfData(data);
+
+            _range = WhenTheRangeIsCalculated();
+
+            ThenItShouldReturnTheExpectedValue(_range, expected);
+        }
+
+        private double WhenTheRangeIsCalculated()
+        {
+            return _dataSet.Range();
+        }
     }
 }
--- StatsNET/Statistic.Basic/DescriptiveStatistics/MeasuresOfDispersionCalculator.cs
+++ StatsNET/Statistic.Basic/DescriptiveStatistics/MeasuresOfDispersionCalculator.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Statistic.Basic.DescriptiveStatistics
 {
     public static class MeasuresOfDispersionCalculator
     {
+        public static double Range(this IList<double> dataset)
+        {
+            var orderedDatasetDescending = dataset.OrderByDescending(d => d).ToList();
+
+            return orderedDatasetDescending.FirstOrDefault() - orderedDatasetDescending[orderedDatasetDescending.Count - 1];
+        }
     }
 }
