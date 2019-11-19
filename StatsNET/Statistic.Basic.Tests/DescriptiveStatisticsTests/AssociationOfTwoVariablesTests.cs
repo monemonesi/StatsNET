1a4f487a245d804832c3a54b4b63e833272148e1
Roberto Monesi <roberto.monesi@outlook.com>
2019-11-19 06:00:49 +0000

    Added AssociationOfTwoVariables classes

---
+++ StatsNET/Statistic.Basic.Tests/DescriptiveStatisticsTests/AssociationOfTwoVariablesTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Statistic.Basic.Tests.DescriptiveStatisticsTests
+{
+    [TestFixture]
+    class AssociationOfTwoVariablesTests : BaseTestClassHelper
+    {
+    }
+}
---
+++ StatsNET/Statistic.Basic/DescriptiveStatistics/AssociationOfTwoVariables.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Statistic.Basic.DescriptiveStatistics
+{
+    public static class AssociationOfTwoVariables
+    {
+
+    }
+}
