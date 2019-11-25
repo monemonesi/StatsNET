c1176a4e38040df399b1cb3ee79c3ec2467218b8
Roberto Monesi <roberto.monesi@outlook.com>
2019-11-25 07:23:37 +0000

    basic implementation of Pearson Correlation Coefficient

---
+++ StatsNET/Statistic.Basic.Tests/DescriptiveStatisticsTests/CorrelationCalculatorTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using Statistic.Basic.DescriptiveStatistics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Statistic.Basic.Tests.DescriptiveStatisticsTests
+{
+    [TestFixture]
+    class CorrelationCalculatorTests : BaseTestClassHelper
+    {
+
+        [TestCase("-0,-1,-2,-3,-4,-5,-6,-7,-8,-9", "0,10,20,30,40,50,60,70,80,90", -1)]
+        [TestCase("0,1,2,3,4,5,6,7,8,9", "0,10,20,30,40,50,60,70,80,90", 1)]
+        public void PearsonCoefficientShouldReturnTheCorrectValue(string data1, string data2, double expected)
+        {
+            _dataSet1 = GivenASetOfData(data1);
+            _dataset2 = GivenASetOfData(data2);
+
+            _result = WhenPearsonCoefficientIsCalculated();
+
+            ThenItShouldReturnTheExpectedValue(_result, expected);
+        }
+
+        private double WhenPearsonCoefficientIsCalculated()
+        {
+            return _dataSet1.Correlation(_dataset2, CorrelationType.Pearson);
+        }
+    }
+}
---
+++ StatsNET/Statistic.Basic/DescriptiveStatistics/CorrelationCalculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Statistic.Basic.DescriptiveStatistics
+{
+    public static class CorrelationCalculator
+    {
+        public static double Correlation(this IList<double> datasetX, IList<double> datasetY, CorrelationType type)
+        {
+            //TODO: Defensive code against possible exceptions (different lenghts, etc..)
+            double meanX = datasetX.Mean();
+            double meanY = datasetY.Mean();
+
+            double numerator = 0.0;
+
+            for (int i = 0; i < datasetX.Count; i++)
+            {
+                double x = datasetX[i];
+                double y = datasetY[i];
+                numerator += (x - meanX) * (y - meanY);
+            }
+
+            double denominatorX = 0.0;
+            double denominatorY = 0.0;
+
+            for (int i = 0; i < datasetX.Count; i++)
+            {
+                double x = datasetX[i];
+                double y = datasetY[i];
+                denominatorX += Math.Pow((x -meanX), 2);
+                denominatorY += Math.Pow((y - meanY), 2);
+            }
+
+            return numerator / Math.Sqrt(denominatorX * denominatorY);
+        }
+    }
+}
