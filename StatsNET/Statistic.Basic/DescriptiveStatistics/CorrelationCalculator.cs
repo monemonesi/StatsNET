5a4fb1e36bb964455ba3ea60c3090dc73afcb8d9
Roberto Monesi <roberto.monesi@outlook.com>
2020-01-02 10:52:17 +0000

    Adding TODO to SpearmanRankCorrelation

--- StatsNET/Statistic.Basic/DescriptiveStatistics/CorrelationCalculator.cs
+++ StatsNET/Statistic.Basic/DescriptiveStatistics/CorrelationCalculator.cs
@@ -27,6 +27,8 @@ namespace Statistic.Basic.DescriptiveStatistics
          
         }
 
+        //TODO: Spearman rank should be calculated using covar formula: https://en.wikipedia.org/wiki/Spearman%27s_rank_correlation_coefficient
+        //TODO: https://en.wikipedia.org/wiki/Covariance , https://en.wikipedia.org/wiki/Ranking#Fractional_ranking_.28.221_2.5_2.5_4.22_ranking.29
         private static double CalculateSpearmanRankCorrelation(IList<double> datasetX, IList<double> datasetY)
         {
             var rankX = datasetX.GetRank();
